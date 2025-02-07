7166e25b4f67b04341a6c72fea0352c5c17f257d
Hashim Aziz Muhammad <haazizjhb024@student.wethinkcode.co.za>
2025-02-07 11:57:56 +0200

    changes tasksmangaer but not complete

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,13 +1,31 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class TaskManager {
     private List<String> tasks; // hint: will change in iteration 3
 
+
     public TaskManager() {
         // Initialize tasks list
         tasks = new ArrayList<>();
+    }
+
+    public static void  ReadTasks(String args) throws IOException {
+        Scanner sc = new Scanner(new File("/home/wt/Documents/hashim/DebugSquad/Javado-hashim/src/main/resources/task_read.csv"));
+        sc.useDelimiter(",");   //sets the delimiter pattern
+        while (sc.hasNext())  //returns a boolean value
+        {
+            String task = Arrays.toString(sc.next().split(","));
+            System.out.println(task);
+
+            //find and returns the next complete token from this scanner
+        }
+
+        sc.close();
     }
 
     public void addTask(String task) {
@@ -29,5 +47,9 @@ public class TaskManager {
 
     public void exit() {
         // leave for iteration 2
+    }
+
+    public static void main(String[] args) throws IOException {
+        ReadTasks("resources/tasks_read.csv");
     }
 }
