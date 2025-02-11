49aa0c6393383676e0cb95d5c4073ebbf0921379
Hashim Aziz Muhammad <haazizjhb024@student.wethinkcode.co.za>
2025-02-11 10:47:06 +0200

    Changes to TaskManager completet iteration 2

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
@@ -9,17 +10,14 @@ public class TaskManager {
     public TaskManager() {
         // Initialize tasks list
         tasks = new ArrayList<>();
-        String data =(ReadTasks("/home/wt/Documents/hashim/DebugSquad/Javado-hashim/src/main/resources/task_read.csv").toString());
-        if(data.isEmpty()){
-            tasks = new ArrayList<>();
-        }
-        tasks.add(data);
+        tasks = (ReadTasks("/home/wt/Documents/hashim/DebugSquad/Javado-hashim/src/main/resources/task_read.csv"));
+        System.out.println(tasks);
     }
 
     public List<String> ReadTasks(String args) {
         try {
             List<String> data = new ArrayList<>();
-             // replace with the path to your own CSV file
+            // replace with the path to your own CSV file
             File file = new File(args);
             Scanner s = new Scanner(file);
 
@@ -27,6 +25,7 @@ public class TaskManager {
                 List<String> lineData = Arrays.asList(s.nextLine().split(","));
                 String lineData1 = lineData.get(1);
                 data.add(lineData1);
+
             }
 
             s.close();
@@ -42,26 +41,58 @@ public class TaskManager {
         tasks.add(task);
     }
 
-    public  List<String> listTasks() {
+    public List<String> listTasks() {
         System.out.println(tasks.size());
 //        return new ArrayList<>(tasks);
-        for (String task: tasks){
+        if (tasks.isEmpty()) {
+            System.out.println("The task list is currently empty. ");
+        }
+        for (String task : tasks) {
 
             System.out.println(task);
         }
         return new ArrayList<>(tasks);
     }
 
-    public void deleteTask(String task){
+    public void deleteTask(String task) {
 //        leave for iteration 4
     }
 
-    public void exit() {
-        // leave for iteration 2
+    public static void writeUsingFileWriter(List<String> data) {
+        File file = new File("/home/wt/Documents/hashim/DebugSquad/Javado-hashim/src/main/resources/task_read.csv");
+        FileWriter fr = null;
+
+        try {
+            fr = new FileWriter(file);
+            int count = 0;
+            for (String item : data) {
+                count += 1;
+                String a = String.format("%d,%s\n", count, item);
+                System.out.print(a);
+                fr.write(a);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            ;
+        } finally {
+            try {
+                assert fr != null;
+                fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
     }
 
-    public static void main(String[] args) throws IOException {
+    public void exit() {
+        writeUsingFileWriter(tasks);
+    }
+
+    public static void main(String[] args) {
         TaskManager taskManager = new TaskManager();
-        taskManager.listTasks();
+
+
     }
 }
